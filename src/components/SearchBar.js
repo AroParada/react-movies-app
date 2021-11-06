7ee246916527f89167736d7c4ebf6bb92f7c974b
harold parada <haroldparada20@gmail.com>
2021-11-06 01:57:06 -0400

    added search button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,19 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="row">
+       <form className="col s12">
         <div className="row">
-          <div className="input-field col s6">
-            <i className="material-icons prefix">search</i>
-              <input id="icon_prefix" type="text" ></input>
+          <div className="input-field col s6 " >
+            <i className="material-icons prefix" >search</i>
+            
+              <input id="icon_prefix" type="text"  ></input>
+              <button class="btn waves-effect waves-light" type="submit" name="action">Search
+              <i class="material-icons right">send</i>
+            </button>
             <label htmlFor="icon_prefix">Search for a movie</label>
           </div>
         </div>
+        </form>
       </div>
     )
   }
