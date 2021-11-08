dcf2a8b643900fd8fe94f9a54b28512423381f81
Aro Parada <haroldparada20@gmail.com>
2021-11-07 23:34:06 -0500

    added link to icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Linkedin } from 'grommet-icons';
+import {Anchor} from 'grommet';
 
 class Footer extends Component {
-
+  
   render() {
     return (
       <footer class="page-footer">
@@ -10,8 +11,7 @@ class Footer extends Component {
             <div  class="container">
             © 2021 Made by Aro Parada
             <a class="grey-text text-lighten-4 right" href="#!">
-              
-              <Linkedin color="white" />
+            <Anchor href="https://www.linkedin.com/in/aroparada/" target="_blank" rel="noopener" color="plain" icon={<Linkedin color="white" />} />
               </a>
             </div>
           </footer>
